ddc94bb49bf679ac86d3c423bd994c5b96cb8558
ghcstyy <89969414+ghcstyy@users.noreply.github.com>
2025-06-30 21:41:44 +0200

    Started to parse the json

--- src/proto-minecraft.c
+++ src/proto-minecraft.c
@@ -10,12 +10,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define JSON_MAX_TOKEN_COUNT 128
+
 static unsigned char handshake[8] = {0x07, 0x00, 0x82, 0x06, 0x00, 0x00, 0x00, 0x01};
 static unsigned char status_request[2] = {0x01, 0x00};
 static unsigned char ping_request[10] = {0x09, 0x01, 0xF0, 0x0D, 0xBA,
                                          0xD0, 0x00, 0x00, 0x00, 0x00};
 
-int str2int(char* s, int length)
+int str2int(const unsigned char* s, int length)
 {
     int out = 0;
     for (int i = 0; i < length; i++)
@@ -26,10 +28,10 @@ int str2int(char* s, int length)
     return out;
 }
 
-int string_compare(const char* json, jsmntok_t* tok, const char* s)
+int compare(const unsigned char* json, jsmntok_t* tok, const char* s)
 {
     return (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
-            strncmp(json + tok->start, s, tok->end - tok->start) == 0);
+            strncmp((const char*) json + tok->start, s, tok->end - tok->start) == 0);
 }
 
 int read_varint(const unsigned char* data, int* value, int* bytes_read)
@@ -75,6 +77,123 @@ int read_packet_information(const unsigned char* packet, int* length, int* id, i
     return 0;
 }
 
+int parse_json(const unsigned char* json, int json_size, struct StreamState* pstate)
+{
+    jsmn_parser parser;
+    jsmntok_t tokens[JSON_MAX_TOKEN_COUNT];
+
+    jsmn_init(&parser);
+
+    int token_count =
+        jsmn_parse(&parser, (const char*) json, json_size, tokens, JSON_MAX_TOKEN_COUNT);
+
+    if (token_count < 0)
+    {
+        LOG(1, "Failed to parse JSON from the status packet!\n");
+        return -1;
+    }
+
+    LOG(1, "Successfully parsed JSON!\n");
+
+    for (int current = 1; current < token_count; current++)
+    {
+        if (compare(json, &tokens[current], "version"))
+        {
+            current++;
+
+            int children = tokens[current].size;
+            for (int i = 0; i < children; i++)
+            {
+                current++;
+
+                if (compare(json, &tokens[current], "name"))
+                {
+                    current++;
+
+                    int size = tokens[current].end - tokens[current].start;
+                    pstate->sub.minecraft.version_name = malloc(size + 1);
+                    memcpy(pstate->sub.minecraft.version_name, json + tokens[current].start, size);
+                    pstate->sub.minecraft.version_name[size] = '\0';
+                    LOG(1, "Version Name: %s\n", pstate->sub.minecraft.version_name);
+                }
+                else if (compare(json, &tokens[current], "protocol"))
+                {
+                    current++;
+
+                    int size = tokens[current].end - tokens[current].start;
+                    pstate->sub.minecraft.version_id = str2int(json + tokens[current].start, size);
+                    LOG(1, "Protocol Version Number: %d\n", pstate->sub.minecraft.version_id);
+                }
+                else
+                {
+                    current++;
+                }
+            }
+        }
+
+        else if (compare(json, &tokens[current], "players"))
+        {
+            current++;
+
+            int children = tokens[current].size;
+            for (int i = 0; i < children; i++)
+            {
+                current++;
+
+                if (compare(json, &tokens[current], "max"))
+                {
+                    current++;
+
+                    int size = tokens[current].end - tokens[current].start;
+                    pstate->sub.minecraft.max_players = str2int(json + tokens[current].start, size);
+                    LOG(1, "Max Players: %d\n", pstate->sub.minecraft.max_players);
+                }
+                else if (compare(json, &tokens[current], "online"))
+                {
+                    current++;
+
+                    int size = tokens[current].end - tokens[current].start;
+                    pstate->sub.minecraft.players_online =
+                        str2int(json + tokens[current].start, size);
+                    LOG(1, "Players Online: %d\n", pstate->sub.minecraft.players_online);
+                }
+            }
+        }
+
+        else if (compare(json, &tokens[current], "description"))
+        {
+            current++;
+
+            int children = tokens[current].size;
+            for (int i = 0; i < children; i++)
+            {
+                current++;
+
+                if (compare(json, &tokens[current], "text"))
+                {
+                    current++;
+
+                    int size = tokens[current].end - tokens[current].start;
+                    pstate->sub.minecraft.description = malloc(size + 1);
+                    memcpy(pstate->sub.minecraft.description, json + tokens[current].start, size);
+                    pstate->sub.minecraft.description[size] = '\0';
+                    LOG(1, "Description: %s\n", pstate->sub.minecraft.description);
+                }
+            }
+        }
+
+        else if (compare(json, &tokens[current], "enforcesSecureChat"))
+        {
+            current++;
+            for (int _ = 0; _ <= tokens[current].size; _++)
+            {
+            }
+        }
+    }
+
+    return 0;
+}
+
 /***************************************************************************
  ***************************************************************************/
 static void minecraft_parse(const struct Banner1* banner1, void* banner1_private,
@@ -92,58 +211,11 @@ static void minecraft_parse(const struct Banner1* banner1, void* banner1_private
         int bytes_read_json_length = 0;
         read_varint(px + read, &json_size, &bytes_read_json_length);
 
-        const char* json = px + read + bytes_read_json_length;
+        const unsigned char* json = px + read + bytes_read_json_length;
 
         banout_append(banout, PROTO_MINECRAFT, json, json_size);
 
-        jsmn_parser p;
-        jsmntok_t tokens[128]; /* We expect no more than 128 JSON tokens */
-
-        jsmn_init(&p);
-        int token_count = jsmn_parse(&p, json, json_size, tokens,
-                                     128);  // "s" is the char array holding the json content
-
-        for (size_t i = 1; i < token_count; i + 2)
-        {
-            jsmntok_t token = tokens[i];
-
-            if (token.type != 4)
-                continue;
-
-            jsmntok_t value = tokens[i + 1];
-
-            if (string_compare(json, &token, "version"))
-            {
-                char* v = malloc(sizeof(value.end - value.start));
-                memcpy(v, json + value.start, value.end - value.start);
-                pstate->sub.minecraft.version_name = v;
-            }
-            if (token.type == 4)
-            {
-                switch (value.type)
-                {
-                    case 1:
-                        LOG(0, "Read OBJECT: %.*s %.*s\n", token.end - token.start,
-                            json + token.start, value.end - value.start, json + value.start);
-                        break;
-                    case 2:
-                        LOG(0, "Read ARRAY: %.*s %.*s\n", token.end - token.start,
-                            json + token.start, value.end - value.start, json + value.start);
-                        break;
-                    case 4:
-                        LOG(0, "Read STRING: %.*s %.*s\n", token.end - token.start,
-                            json + token.start, value.end - value.start, json + value.start);
-                        break;
-                    case 8:
-                        LOG(0, "Read PRIMITIVE: %.*s %.*s\n", token.end - token.start,
-                            json + token.start, value.end - value.start, json + value.start);
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-        }
+        parse_json(json, json_size, pstate);
     }
 
     return;
